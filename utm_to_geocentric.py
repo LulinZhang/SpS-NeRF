7e883cb38b5eb86d723c0b550c60b4ec6f80ce31
Lulin Zhang <53054135+LulinZhang@users.noreply.github.com>
2023-08-08 09:16:59 +0200

    Add files via upload

--- utm_to_geocentric.py
+++ utm_to_geocentric.py
@@ -29,9 +29,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--file_dir', type=str, required=True,
                         help='file_dir')
-    parser.add_argument('--zonestring', type=str, default='17',
-                        help='zonestring')
+    parser.add_argument('--aoi_id', type=str, default='JAX_214',
+                        help='aoi_id')
     args = parser.parse_args()
+
+    zonestring = "none"
+    if args.aoi_id[:3] == "JAX":
+        zonestring = "17" #"17R"
+    elif args.aoi_id[:3] == "Dji":
+        zonestring = "38" #"38N"
+    else:
+        raise ValueError("AOI not valid. Expected JAX_xxx or Dji_xxx but received {}".format(aoi_id))
 
     densedepth_files = sorted(glob.glob(args.file_dir + "/*_3DPts.txt"))
 
@@ -39,5 +47,5 @@ if __name__ == '__main__':
         print('--------------', t, densedepth_file, '--------------')
         inFile = densedepth_file
         outFile = densedepth_file[:-4] + "_ecef.txt"
-        utm_to_geocentric(inFile, outFile, args.zonestring)
+        utm_to_geocentric(inFile, outFile, zonestring)
 
